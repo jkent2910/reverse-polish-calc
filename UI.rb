a80190c2463b8a81e567fbbf37b7ea2d556c0cc0
Julie Kent <juliekent@Julies-MacBook-Pro-2.local>
2016-07-15 13:27:57 -0500

    adding a couple of msgs for calc and changing method params to make testing easier

--- UI.rb
+++ UI.rb
@@ -6,11 +6,20 @@ class UI
     self.output = $stdout
   end
 
-  def give(output_message)
+  def give(output_message, output: @output)
     output.puts output_message
   end
 
-  def receive
+  def receive(input: @input)
     @user_input = input.gets.chomp
   end
+
+  def give_welcome_message
+    give("Welcome to Julie's Reverse Polish Calculator!  Type 'c' to continue or 'q' to exit.")
+  end
+
+  def give_operator_message
+    give("Please enter a number with no more than 5 digits or an operator (+ - / or *).  Press 'q' to exit.")
+  end
+
 end
